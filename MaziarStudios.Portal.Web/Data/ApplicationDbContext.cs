a260afedf185db3ea4eb6ac4ae0a2f8191814efc
Robert Maziar <robert.maziar@gmail.com>
2020-03-30 15:40:04 -0400

    Setup Role CRUD Operations
    Necessary changes made to setup CRUD operations on User Roles.

--- MaziarStudios.Portal.Web/Data/ApplicationDbContext.cs
+++ MaziarStudios.Portal.Web/Data/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MaziarStudios.Portal.Web.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MaziarStudios.Portal.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- MaziarStudios.Portal.Web/Startup.cs
+++ MaziarStudios.Portal.Web/Startup.cs
@@ -12,6 +12,7 @@ using MaziarStudios.Portal.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MaziarStudios.Portal.Web.Models;
 
 namespace MaziarStudios.Portal.Web
 {
@@ -30,7 +31,8 @@ namespace MaziarStudios.Portal.Web
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
