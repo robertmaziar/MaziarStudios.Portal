60cff986c325c0cf26410b38431a0bd87eef70f9
Robert Maziar <robert.maziar@gmail.com>
2020-04-01 10:50:25 -0400

    Add Events Controller and Views
    Added events controller and views through scaffolding.

--- MaziarStudios.Portal.Web/Data/Migrations/20200331191416_Event_EventArtist.Designer.cs
+++ MaziarStudios.Portal.Web/Data/Migrations/20200331191416_Event_EventArtist.Designer.cs
@@ -4,14 +4,16 @@ using MaziarStudios.Portal.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MaziarStudios.Portal.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200331191416_Event_EventArtist")]
+    partial class Event_EventArtist
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MaziarStudios.Portal.Web/Data/Migrations/20200331191416_Event_EventArtist.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MaziarStudios.Portal.Web.Data.Migrations
+{
+    public partial class Event_EventArtist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientId = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Start = table.Column<DateTime>(nullable: false),
+                    End = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Event_AspNetUsers_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventArtist",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(nullable: false),
+                    ArtistId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventArtist", x => new { x.EventId, x.ArtistId });
+                    table.ForeignKey(
+                        name: "FK_EventArtist_AspNetUsers_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventArtist_Event_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Event",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_ClientId",
+                table: "Event",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventArtist_ArtistId",
+                table: "EventArtist",
+                column: "ArtistId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventArtist");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+        }
+    }
+}
